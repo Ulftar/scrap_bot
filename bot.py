bf39aa89b44b9741195875a280145822b586f3d6
Ulftar <alexandr.kolodnitskiy@gmail.com>
2022-11-17 16:01:22 +0700

    Add tg bot test launch

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters import Text
+import auth_data
 
 """Создание объекта бота"""
-bot = Bot(token='TOKEN')
+bot = Bot(token=auth_data.token)
 """Создание объекта диспетчера для управления хэндлерами"""
 dp = Dispatcher(bot)
 
@@ -11,3 +12,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
     await message.answer('Hello!')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
